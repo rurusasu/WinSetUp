c2c5eeae7c19ed4a685e842100aca61238ead70f
rurusasu <rurusasu153@gmail.com>
2023-04-26 21:32:13 +0900

    ファイル整理

--- Win11Setup/Win11Setup.ps1
+++ Win11Setup/Win11Setup.ps1
@@ -2,7 +2,6 @@ Set-Location -Path C:\
 
 # 作業ディレクトリを保持しつつ管理者としてPowershellスクリプトを実行する
 # REF: https://www.delftstack.com/ja/howto/powershell/powershell-run-as-administrator/
-
 if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
         Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
